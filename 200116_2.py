c06ff3bda1bbdec3ee02556f1a59f72c969022dd
Jeong Haeyeon <jeonghy0709@naver.com>
2020-01-16 23:11:40 +0900

    200116_2.py

---
+++ 200116_2.py
@@ -0,0 +1,15 @@
+#import tensorflow as tf
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b
+
+sess = tf.Session()
+
+print(sess.run(adder_node, feed_dict={a: 3, b: 4.5}))
+print(sess.run(adder_node, feed_dict={a: [1, 3], b: [2, 4]}))
+
