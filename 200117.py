f744d918d5d9006e5334ee5b9e63864b406c1df4
Jeong Haeyeon <jeonghy0709@naver.com>
2020-01-17 22:33:49 +0900

    200117.py

---
+++ 200117.py
@@ -0,0 +1,34 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+# X and Y data
+x_train = [1, 2, 3]
+y_train = [1, 2, 3]
+
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Our hypothesis XW+b
+hypothesis = x_train * W + b
+
+# cost/loss function
+'''
+    t = [1., 2., 3., 4.]
+    tf.reduce_mean(t) ==> 2.5
+'''
+cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+
+# Minimize
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+# Launch the graph in a session.
+sess = tf.Session()
+# Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+
+# Fit the line
+for step in range(2001):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(cost), sess.run(W), sess.run(b))
